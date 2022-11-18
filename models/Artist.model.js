bc984e3e93cb725a1875bbab1178b0b3cec9b2af
Baptiste LAVAL <baptiste.laval@edu.univ-fcomte.fr>
2022-11-18 09:46:31 +0100

    creation du model Artist et de trois fonction sur artiste

---
+++ models/Artist.model.js
@@ -0,0 +1,35 @@
+import {DataTypes} from sequelize ;
+import db from '../db/db.js' ;
+
+const Artist = db.define('admin',{
+    idartist :{
+        type : DataTypes.INTEGER,
+        primaryKey : true ,
+        serial : true ,
+        allowNull : false ,
+    },
+    name :{
+        type : DataTypes.STRING ,
+        allowNull : false ,
+    }, 
+    image :{
+        type : DataTypes.STRING,
+        allowNull : false,
+    },
+    biography :{
+        type : DataTypes.TEXT ,
+        allowNull : false ,
+    },
+    id_genre :{
+        type : DataTypes.INTEGER,
+        allowNull : false ,
+        foreignKey : true ,
+        references : { 
+            model : 'genre' , 
+            key : 'id_genre'
+        },
+    }, 
+}, {timestamps:false , freezeTableName:true});
+    
+export default Admin ;
+
--- services/admin.service.js
+++ services/admin.service.js
@@ -1,8 +1,10 @@
 import {client} from "../server.js";
+import Artist from "../models/Artist.model.js";
+import { create } from "express-handlebars";
 
 const test = (callback) => {
     try {
-        return callback(null, "test admin réussi");
+        return callback(null, "test Artist réussi");
     }catch (e) {
         console.log("error test");
         console.log(e);
@@ -10,12 +12,27 @@ const test = (callback) => {
     }
 }
 
-const find = (body, callback) => {
-    //TODO
+const get = ( id, callback) => {
+    Ar.findAll().then(data=>{
+        return callback(null , data);
+    }).catch (e => {
+        return callback({locationError: "findAllArtist" , error: e.message }, null);
+    });
+}
+const getById = ( id, callback) => {
+    Artist.findByPk(id).then(data=>{
+        return callback(null , data);
+    }).catch (e => {
+        return callback({locationError: "findArtist" , error: e.message }, null);
+    });
 }
 
 const add = (body, callback) => {
-    //TODO
+    Artist.create(body).then(data=>{
+        return callback(null, data);
+    }).catch(e =>{
+    return callback({locationError: "addArtist" , error: e.message }, null);
+});
 }
 
 const del = (params, callback) => {
