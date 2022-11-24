dba33f5ca6b583841f5afed576b42b0f7c646add
AntoinePR1 <titounet.perrin@gmail.com>
2022-11-24 11:22:26 +0100

    Ajout models  6 et 7 et correction nationality

---
+++ models/TypePrestataire.models.js
@@ -0,0 +1,18 @@
+import { DataTypes } from "sequelize";
+import db from '../db/db.js';
+
+const TypePrestataire = db.define('typeprestataire', {
+    id_typeprestataire: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        serial: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    libelle_typeprestataire: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }
+}, {timestamps: false, freezeTableName: true});
+
+export default TypePrestataire;
