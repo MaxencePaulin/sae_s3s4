e4b7bac1901a2f0b890676d1af832136141dee22
Baptiste LAVAL <baptiste.laval@edu.univ-fcomte.fr>
2022-11-25 09:45:04 +0100

    creation controller Place

---
+++ controllers/place.controller.js
@@ -0,0 +1,89 @@
+import Place from '../models/Place.model.js';
+
+export const findAll = (req, res) => {
+    Place.findAll().then(data => {
+        res.send(data);
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const findOne = (req, res) => {
+    const id = parseInt(req.params.id);
+    Place.findByPk(id).then(data => {
+        res.send(data);
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const create = (req, res) => {
+    const body = req.body;
+    Place.create(body).then(data => {
+        res.send(data);
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const update = (req, res) => {
+    const id = parseInt(req.params.id);
+    const body = req.body;
+    Place.update(body, {
+        where: { id: id }
+    }).then(data => {
+        if (data === 1) {
+            res.send({
+                message: "Place was updated successfully."
+            });
+        } else {
+            res.send({
+                message: `Cannot update Place with id=${id}. Maybe Place was not found or req.body is empty!`
+            });
+        }
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const remove = (req, res) => {
+    const id = parseInt(req.params.id);
+    Place.destroy({
+        where: { id: id }
+    }).then(data => {
+        if (data === 1) {
+            res.send({
+                message: "Place was deleted successfully!"
+            });
+        } else {
+            res.send({
+                message: `Cannot delete Place with id=${id}. Maybe Place was not found!`
+            });
+        }
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const removeAll = (req, res) => {
+    Place.destroy({
+        where: {},
+        truncate: false
+    }).then(data => {
+        res.send({ message: `${data} Place were deleted successfully!` });
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
