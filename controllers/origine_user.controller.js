d4f5c0f034abd7e5cd0b6ae67a945409c5a5352c
Baptiste LAVAL <baptiste.laval@edu.univ-fcomte.fr>
2022-11-25 09:36:34 +0100

    creation controller origineuser

---
+++ controllers/origine_user.controller.js
@@ -0,0 +1,89 @@
+import Origineuser from '../models/Origineuser.model.js';
+
+export const findAll = (req, res) => {
+    Origineuser.findAll().then(data => {
+        res.send(data);
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const findOne = (req, res) => {
+    const id = parseInt(req.params.id);
+    Origineuser.findByPk(id).then(data => {
+        res.send(data);
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const create = (req, res) => {
+    const body = req.body;
+    Origineuser.create(body).then(data => {
+        res.send(data);
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const update = (req, res) => {
+    const id = parseInt(req.params.id);
+    const body = req.body;
+    Origineuser.update(body, {
+        where: { id: id }
+    }).then(data => {
+        if (data === 1) {
+            res.send({
+                message: "Origineuser was updated successfully."
+            });
+        } else {
+            res.send({
+                message: `Cannot update Origineuser with id=${id}. Maybe Origineuser was not found or req.body is empty!`
+            });
+        }
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const remove = (req, res) => {
+    const id = parseInt(req.params.id);
+    Origineuser.destroy({
+        where: { id: id }
+    }).then(data => {
+        if (data === 1) {
+            res.send({
+                message: "Origineuser was deleted successfully!"
+            });
+        } else {
+            res.send({
+                message: `Cannot delete Origineuser with id=${id}. Maybe Origineuser was not found!`
+            });
+        }
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const removeAll = (req, res) => {
+    Origineuser.destroy({
+        where: {},
+        truncate: false
+    }).then(data => {
+        res.send({ message: `${data} Origineuser were deleted successfully!` });
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
