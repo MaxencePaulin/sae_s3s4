8e367dc89df685fa373fffec58f982593925713a
Baptiste LAVAL <baptiste.laval@edu.univ-fcomte.fr>
2022-11-25 08:29:15 +0100

    creation controllers Bought

---
+++ controllers/bought.controller.js
@@ -0,0 +1,89 @@
+import Bought from '../models/Bought.model.js';
+
+export const findAll = (req, res) => {
+    Bought.findAll().then(data => {
+        res.send(data);
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const findOne = (req, res) => {
+    const id = parseInt(req.params.id);
+    Bought.findByPk(id).then(data => {
+        res.send(data);
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const create = (req, res) => {
+    const body = req.body;
+    Bought.create(body).then(data => {
+        res.send(data);
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const update = (req, res) => {
+    const id = parseInt(req.params.id);
+    const body = req.body;
+    Bought.update(body, {
+        where: { id: id }
+    }).then(data => {
+        if (data === 1) {
+            res.send({
+                message: "Bought was updated successfully."
+            });
+        } else {
+            res.send({
+                message: `Cannot update Bought with id=${id}. Maybe Bought was not found or req.body is empty!`
+            });
+        }
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const remove = (req, res) => {
+    const id = parseInt(req.params.id);
+    Bought.destroy({
+        where: { id: id }
+    }).then(data => {
+        if (data === 1) {
+            res.send({
+                message: "Bought was deleted successfully!"
+            });
+        } else {
+            res.send({
+                message: `Cannot delete Bought with id=${id}. Maybe Bought was not found!`
+            });
+        }
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const removeAll = (req, res) => {
+    Bought.destroy({
+        where: {},
+        truncate: false
+    }).then(data => {
+        res.send({ message: `${data} Bought were deleted successfully!` });
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
