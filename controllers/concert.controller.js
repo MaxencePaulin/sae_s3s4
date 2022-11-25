044f0eda7bc1bbf32774cc384d46f9dc9879b982
Baptiste LAVAL <baptiste.laval@edu.univ-fcomte.fr>
2022-11-25 08:38:28 +0100

    creation controller concert

---
+++ controllers/concert.controller.js
@@ -0,0 +1,89 @@
+import Concert from '../models/Concert.model.js';
+
+export const findAll = (req, res) => {
+    Concert.findAll().then(data => {
+        res.send(data);
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const findOne = (req, res) => {
+    const id = parseInt(req.params.id);
+    Concert.findByPk(id).then(data => {
+        res.send(data);
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const create = (req, res) => {
+    const body = req.body;
+    Concert.create(body).then(data => {
+        res.send(data);
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const update = (req, res) => {
+    const id = parseInt(req.params.id);
+    const body = req.body;
+    Concert.update(body, {
+        where: { id: id }
+    }).then(data => {
+        if (data === 1) {
+            res.send({
+                message: "Concert was updated successfully."
+            });
+        } else {
+            res.send({
+                message: `Cannot update Concert with id=${id}. Maybe Concert was not found or req.body is empty!`
+            });
+        }
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const remove = (req, res) => {
+    const id = parseInt(req.params.id);
+    Concert.destroy({
+        where: { id: id }
+    }).then(data => {
+        if (data === 1) {
+            res.send({
+                message: "Concert was deleted successfully!"
+            });
+        } else {
+            res.send({
+                message: `Cannot delete Concert with id=${id}. Maybe Concert was not found!`
+            });
+        }
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
+
+export const removeAll = (req, res) => {
+    Concert.destroy({
+        where: {},
+        truncate: false
+    }).then(data => {
+        res.send({ message: `${data} Concert were deleted successfully!` });
+    }).catch(e => {
+        res.status(500).send({
+            message: e.message || "Some error occurred."
+        });
+    });
+}
