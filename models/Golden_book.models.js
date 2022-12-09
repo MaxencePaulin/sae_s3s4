e6f06ad13dd2096632aa4aff4222ca437569e7e1
Baptiste LAVAL <baptiste.laval@edu.univ-fcomte.fr>
2022-12-09 09:05:11 +0100

    creation model Golden_book

---
+++ models/Golden_book.models.js
@@ -0,0 +1,43 @@
+import { DataTypes } from 'sequelize';
+import db from '../db/db.js';
+
+const Golden_book = db.define('golden_book', {
+    id_avis: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        serial: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    libelle_avis: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    id_artist: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+        references: {
+            model: 'artist',
+            key: 'id_artist',
+        }
+    },
+    id_prestataire: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+        references: {
+            model: 'prestataire',
+            key: 'id_prestataire',
+        }
+    },
+    id_user: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+        references: {
+            model: 'users',
+            key: 'id_users',
+        }
+    }
+    
+}, {timestamps: false, freezeTableName: true});
+
+export default Golden_book;
