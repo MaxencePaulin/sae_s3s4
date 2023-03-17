e1b59da85c58e54db1ca19a167f23429076b954d
maxime <maxime.theveneau@gmail.com>
2023-03-17 10:42:59 +0100

    update Response-Caching

--- middleware/cache.middleware.js
+++ middleware/cache.middleware.js
@@ -1,18 +1,34 @@
 import NodeCache from "node-cache";
 const cache = new NodeCache({useClones:false});
-const COLOR = '\u001B[32m'
+
+const COLOR_GREEN = '\u001B[32m'
+const COLOR_RED = '\u001B[31m'
 const COLOR_RESET = '\u001B[0m'
 
 export default duration => (req,res,next) => {
     if (req.method !== "GET") return next();
+    /*Normally, lastModified retrieves the last modified date of a resource but for this example we configure it with the variable checked:
+     - TRUE : lastModified will be the same as ifModifiedSince Header (same Date).
+     - FALSE : lastModified will be different from ifModifiedSince Header (lastModified's Date more recent than ifModifiedSince's Date => must update cache with new resources).
+     */
+    // TODO change checked
+    const checked = true
+    const ifModifiedSince = req.headers["if-modified-since"]
+    const lastModified = checked ? ifModifiedSince : new Date()
+
+    // Find Cache resource
     const key = req.originalUrl;
     const cachedResponse = cache.get(key);
 
-    if (cachedResponse) {
-        console.log(`${COLOR}[CACHE] HIT:${key}${COLOR_RESET}`)
+    // Cache hit and with last resource
+    if (cachedResponse && (ifModifiedSince === lastModified)) {
+        console.log(`${COLOR_GREEN}[CACHE:HIT] ${key}${COLOR_RESET}`)
         res.send(cachedResponse);
-    } else {
-        console.log(`${COLOR}[CACHE] MISS:${key}${COLOR_RESET}`)
+    }
+    // No Cache or Cache with an older resource
+    else {
+        let cacheError = cachedResponse ? `${COLOR_RED}[CACHE:IF-MODIFIED-SINCE] ${key}${COLOR_RESET}` : `${COLOR_RED}[CACHE:MISS] ${key}${COLOR_RESET}`
+        console.log(cacheError)
         res.originalSend = res.send;
         res.send = body => {
             res.originalSend(body);
